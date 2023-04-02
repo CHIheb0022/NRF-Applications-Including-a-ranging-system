01373394728d7d467e20b334a7ff4c32aba1d925
IHEEB22 <chihebeddine.hmida@ensi-uma.tn>
2023-04-02 06:13:03 +0200

    TWR technique
    I have tried two methode :
    A- using a full duplex pipe
    B- using two pipe to support a bi-directional comunication

--- Distance using ToF_TWR_ one pipe/Node1/Node1.ino
+++ Distance using ToF_TWR_ one pipe/Node1/Node1.ino
@@ -9,7 +9,7 @@
 RF24 radio(pinCE, pinCSN);    // Instanciation du NRF24L01
 
 const byte adresse[6] = tunnel;       // Mise au format "byte array" du nom du tunnel
-char message[16];                     // Avec cette librairie, on est "limité" à 32 caractères par message
+const char* message;                     // Avec cette librairie, on est "limité" à 32 caractères par message
 unsigned long sendTime;
 unsigned long recievedTime;
 unsigned long Distance;
@@ -24,7 +24,6 @@ void setup() {
   // Partie NRF24
   radio.begin();                      // Initialisation du module NRF24
   radio.setChannel(100);               // Sélection du channal 100 correspond à la fréquence 2.410 Ghz (pour éviter les interferance avec BLE and WIFI)
-  radio.openReadingPipe(0, adresse);  // Ouverture du tunnel en LECTURE, avec le "nom" qu'on lui a donné
   radio.openWritingPipe(adresse);     // Ouverture du tunnel en ECRITURE
   radio.setPALevel(RF24_PA_MIN);      // Sélection d'un niveau "MINIMAL" pour communiquer (pas besoin d'une forte puissance, pour nos essais)
   radio.setPayloadSize(32);           // In order to provide a Full-Duplex communication in PIPE1(set payload to 32)
@@ -36,18 +35,19 @@ void loop() {
   message="Hello Node2!";
   sendTime=micros();// Envoyer à la Noeud2 un message // Timer Starter
   radio.write(&message,sizeof(message));
-  Serial.println("From Node1 to Node2 : " + message);
+  Serial.print("From Node1 to Node2 : ");Serial.println(message);
+  radio.openReadingPipe(0, adresse);  // Ouverture du tunnel en LECTURE, avec le "nom" qu'on lui a donné
   // Basculer en mode lecture
   radio.startListening(); 
-  if (!radio.available()) {                      
-    Serial.println("Nothing is received"); 
-    delayMicroseconds(1);                         
+  if (radio.available()) {                      
+    // Recevoir de la Noeud2 un message pour acquisé la reception // Timer Stop
+    sendTime=micros(); 
+    radio.read(&message,sizeof(message)); // Récuperer le message
+    Serial.print("From Node2 to Node1 : ");Serial.println(message);
   }
-  // Recevoir de la Noeud2 un message pour acquisé la reception // Timer Stop
-  radio.read(&message,sizeof(message));
-  sendTime=micros(); 
-  Serial.println("From Node2 to Node1 : " + message);
   ToF2=recievedTime-sendTime;
   Distance=(ToF2*0.0000001*Speed_OF_Light)/2; // Dividing by two becaude ToF2 corresponds to time needed for the two way data exchage
   Serial.print("The distance between the tow nodes is :");Serial.print(String(Distance));Serial.println(" m");
+  delay(1000);
+  radio.stopListening();   
 }
--- Distance using ToF_TWR_ one pipe/Node2/Node2.ino
+++ Distance using ToF_TWR_ one pipe/Node2/Node2.ino
@@ -9,7 +9,7 @@
 RF24 radio(pinCE, pinCSN);    // Instanciation du NRF24L01
 
 const byte adresse[6] = tunnel;       // Mise au format "byte array" du nom du tunnel
-char message[32];                     // Avec cette librairie, on est "limité" à 32 caractères par message
+const char* message;                     // Avec cette librairie, on est "limité" à 32 caractères par message
 unsigned long sendTime;
 unsigned long recievedTime;
 unsigned long Distance;
@@ -25,7 +25,6 @@ void setup() {
   radio.begin();                      // Initialisation du module NRF24
   radio.setChannel(100);               // Sélection du channal 100 correspond à la fréquence 2.410 Ghz (pour éviter les interferance avec BLE and WIFI)
   radio.openReadingPipe(0, adresse);  // Ouverture du tunnel en LECTURE, avec le "nom" qu'on lui a donné
-  radio.openWritingPipe(adresse);     // Ouverture du tunnel en ECRITURE
   radio.setPALevel(RF24_PA_MIN);      // Sélection d'un niveau "MINIMAL" pour communiquer (pas besoin d'une forte puissance, pour nos essais)
   radio.setPayloadSize(32);           // In order to provide a Full-Duplex communication in PIPE1(set payload to 32)
   radio.startListening();             // Démarrage de l'écoute du NRF24 (signifiant qu'on va recevoir, et non émettre quoi que ce soit, ici)
@@ -36,11 +35,15 @@ void loop() {
   // Recevoir à partir de la Noeud1 
   if (radio.available()){
     radio.read(&message,sizeof(message));
+    Serial.print("Message Recieved From Node1 saying");Serial.println(message);
   }
-  Serial.println("Message Recieved From Nod1");
+  radio.openWritingPipe(adresse);     // Ouverture du tunnel en ECRITURE
+  message="Hi!!";
   // Basculer en mode écriture
   radio.stopListening();  
-  // Il faut extraire le retard resultant de cette instruction
-  radio.write(&message=,sizeof(message));   
-  Serial.println("Acknologment sent to node 1 saying " + message); // Pour acqusé la reception
+  // Il faut extraire le retard resultant de ces deux instructions
+  radio.write(&message,sizeof(message));   
+  Serial.println("Acknologment sent to node 1"); // Pour acqusé la reception
+  delay(5000);
+  radio.stopListening();
 }
